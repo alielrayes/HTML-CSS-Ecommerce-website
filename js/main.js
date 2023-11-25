413d576c51c5a16371f6ac007a2dc844eacf08b6
alielrayes <engali7assan@yahoo.com>
2023-11-25 22:07:44 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    headerElement.classList.add("scroll-down")
+  } else {
+    headerElement.classList.remove("scroll-down")
+  }
+})
+
+
+
+
